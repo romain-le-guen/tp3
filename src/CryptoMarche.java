a2780b6a3585d63e351ace91fdf4759e0e3b2063
Romain Le Guen <romain.le-guen@etu.univ-lehavre.fr>
2021-05-27 11:26:06 +0200

    maj 1 de CryptoMarche

--- src/CryptoMarche.java
+++ src/CryptoMarche.java
@@ -24,11 +24,17 @@ public class CryptoMarche {
      * @param proprietare
      * @return capital en euros du propriétare.
      */
-    public double capitalEnEuros(String proprietaire){
-        /**
-			FONCTION À IMPLEMENTER
-        **/
-
+    public double capitalEnEuros(String proprietaire)
+    {
+		double capital = 0;
+        if (this.portefeuilles.size()>0){
+            for(Portefeuille oui: this.portefeuilles)
+            {
+            	if (oui.estProprietaire(proprietaire))
+                	capital=oui.getMontant() * oui.getMonnaie().getValeurDeJeton();
+            }
+            return capital;
+        }
         return 0;
     }
 
@@ -39,11 +45,17 @@ public class CryptoMarche {
      * @param monnaie
      * @return capital total en circulation de la cryptomonnaie (en euros).
      */
-    public double capitalMonneaie(Cryptomonnaie monnaie){
-        /**
-			FONCTION À IMPLEMENTER
-        **/
-
+    public double capitalMonneaie(Cryptomonnaie monnaie)
+    {
+        double capital = 0;
+        if (this.portefeuilles.size()>0){
+            for(Portefeuille oui: this.portefeuilles)
+            {
+            	if (oui.getMonnaie().getNom()== monnaie.getNom())
+                	capital=oui.getMontant() * oui.getMonnaie().getValeurDeJeton();
+            }
+            return capital;
+        }
         return 0;
 
     }
